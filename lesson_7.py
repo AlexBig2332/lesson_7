3cdca423b357312da766e27780088c8f2a0906f3
alexbig2332 <alexbig2332@gmail.com>
2020-02-29 22:00:09 +0300

    HW_7

--- lesson_7.py
+++ lesson_7.py
@@ -5,6 +5,7 @@ with open('car_inf.txt') as f:
         car_inf.append(list)
 car_inf_report_1 = car_inf[1]
 print(car_inf_report_1)
+
 # docx файл
 from docxtpl import DocxTemplate
 
@@ -20,15 +21,14 @@ def from_template(Brand, Model, Engine_Volume, Price, template):
     template = DocxTemplate(template)
     data = get_context(Brand, Model, Engine_Volume, Price)
     template.render(data)
-    template.save('car_inf.docx')
+    template.save('Auto' + '_car_inf.docx')
 
 def report(Brand, Model, Engine_Volume, Price):
-    template = 'car_inf_template.docx'
+    template = 'car_inf.docx'
     document = from_template(Brand, Model, Engine_Volume, Price, template)
 
 
 report(car_inf_report_1[0], car_inf_report_1[1], car_inf_report_1[2], car_inf_report_1[3])
-
 # csv файл
 import csv
 
